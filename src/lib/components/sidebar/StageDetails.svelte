c77f62db5fda01f4677d0233e72012ae2ce7af43
TheBrenny <iam@justbrenny.me>
2025-04-17 01:11:30 +0930

    hack fix

--- src/lib/components/sidebar/StageDetails.svelte
+++ src/lib/components/sidebar/StageDetails.svelte
@@ -7,10 +7,21 @@
     export let open = true;
 
     // BUG: When we dynamically chage the size of child panels, this panel doesn't get updated!
+    // HACK: We are fixing this by pulling the jobs out of the stage for now...
 </script>
 
-<Details {node} {open}>
+<div class="stage">
+    <Details {node} {open}></Details>
     {#each node.data.children as child}
         <JobDetails node={child} open={false} />
     {/each}
-</Details>
+</div>
+
+<style>
+    .stage {
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
+        width: 100%;
+    }
+</style>
